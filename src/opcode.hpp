d45b2f493f9ea2cb27d0ff08381a71a857a063ab
Massimo Bianchi <bianchi.massimo@gmail.com>
2020-06-22 23:26:10 +0200

    some opcode

--- src/opcode.hpp
+++ src/opcode.hpp
@@ -12,8 +12,13 @@ class State;
 class Opcode {
 public:
     enum {
-        CALL_OPCODE = 0x2000,
-        MVI_OPCODE = 0xA000,
+        DISPATCH0_OPCODE = 0x0000,
+        JMP_OPCODE = 0x1000,   //!< Jumps to address NNN.
+        CALL_OPCODE = 0x2000,  //!< Calls subroutine at NNN.
+        EQ_OPCODE = 0x3000,    //!< Skips the next instruction if VX equals NN.
+        NEQ_OPCODE = 0x4000,   //!< Skips the next instruction if VX doesn't equal NN.
+        CMP_OPCODE = 0x5000,   //!< Skips the next instruction if VX equals VY (5XY0).
+        MVI_OPCODE = 0xA000,   //!< Sets Index Register to the address NNN.
     };
 
     explicit Opcode(const std::string nmemonic, word code);
