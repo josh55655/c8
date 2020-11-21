d35382d8551c53a2eeb20e3a0ff1ae2f825ae971
iceseyes <bianchi.massimo@gmail.com>
2020-11-21 14:48:17 +0100

    added program options

--- src/dch8.cpp
+++ src/dch8.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iomanip>
 
+#include <boost/program_options.hpp>
+
 #include "opcode.hpp"
 #include "state.hpp"
 
@@ -20,12 +22,25 @@ using std::vector;
 
 using namespace chip8;
 
+namespace po = boost::program_options;
+
+namespace {
+
+string filename;
+bool displayUnknown{false};
+
+void parseArgs(int argc, char *argv[]);
 void load(const std::string &file);
 
+}  // namespace
+
 int main(int argc, char *argv[]) {
-    load(argv[1]);
+    parseArgs(argc, argv);
+    load(filename);
     return 0;
 }
+
+namespace {
 
 void load(const std::string &_programFile) {
     vector<byte> program;
@@ -46,7 +61,7 @@ void load(const std::string &_programFile) {
                 cout << "0x" << setfill('0') << hex << setw(2) << line << dec << setfill(' ') << "\t"
                      << opcode.toString(_word & 0x0FFF) << "\t\t## 0x" << setw(4) << setfill('0') << hex << _word << dec
                      << setfill(' ') << endl;
-            else
+            else if (displayUnknown)
                 cout << "0x" << setfill('0') << hex << setw(2) << line << dec << setfill(' ') << "\t\t\t\t## 0x"
                      << setw(4) << setfill('0') << hex << _word << dec << setfill(' ') << endl;
 
@@ -54,3 +69,38 @@ void load(const std::string &_programFile) {
         }
     }
 }
+
+void parseArgs(int argc, char *argv[]) {
+    // Declare the supported options.
+    po::options_description desc("De-assembler for chip8 ");
+    po::positional_options_description pd;
+    po::variables_map vm;
+
+    // clang-format off
+    desc.add_options()
+        ("help", "produce help message")
+        ("show-unknown,u", "report unknown bytes")
+        ("file", po::value<string>()->required(), "file to decode");
+    // clang-format on
+
+    pd.add("file", 1);
+    try {
+        auto parsed = po::command_line_parser(argc, argv).options(desc).positional(pd).run();
+
+        po::store(parsed, vm);
+        po::notify(vm);
+    } catch (po::required_option &e) {
+        cout << desc << "\n";
+        exit(1);
+    }
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        exit(1);
+    }
+
+    displayUnknown = vm.count("show-unknown");
+    filename = vm["file"].as<string>();
+}
+
+}  // namespace
